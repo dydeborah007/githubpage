ff4f0441297f49f1b94215c409dbf99c7eab221b
dydeborah007 <92194966+dydeborah007@users.noreply.github.com>
2022-02-12 20:52:33 +0700

    Add mainpage

---
+++ index.js
@@ -0,0 +1,7 @@
+function myContact(){
+    var numberContact = 0712456767;
+   document.getElementById("Num").innerHTML = "Number:" +numberContact;
+   var email = "eatmore@gmail.com";
+   document.getElementById("em").innerHTML = "Email: " +email;
+  
+}
